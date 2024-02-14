8a16402b1e8063e5615ac33daab9f4e61ba8609d
VigneshK <104199131+vigneshk2110@users.noreply.github.com>
2024-02-14 08:14:17 +0530

    Day 1 Files Uploaded

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld{
+ 	public static void main(String[] args){
+
+		int marks = 45;
+		
+	if(marks > 50){
+           System.out.println("Pass");	
+	}
+	else {
+		System.out.println("Fail");
+	}	
+
+		
+	}
+	
+}
